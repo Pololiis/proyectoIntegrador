c3f1b9cffd20ae50e55439a118df9b6299c45a07
Pololiis <pviloriasilva@gmail.com>
2024-05-10 09:30:00 -0400

    Inicializando backend del proyecto integrador.

---
+++ src/main/java/com/proyectoIntegrador/gameShare/GameShareApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoIntegrador.gameShare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameShareApplication.class, args);
+	}
+
+}

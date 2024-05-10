f94d56f1e16569b014b2e50bef47919aa17eb9ca
TomasTozzi <tozzitomas@gmail.com>
2024-05-10 15:24:13 -0300

    estructura header lista falta elemento ux/ui

--- integrador-DH/src/App.jsx
+++ integrador-DH/src/App.jsx
@@ -1,27 +1,23 @@
-import {Route, Routes, BrowserRouter} from "react-router-dom"
-import Layout from "./components/layout/Layout"
-import Home from "./components/routes/Home"
-import Tienda from "./components/routes/About"
-import Carrito from "./components/routes/Contact"
-
-
-import './App.css'
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import Home from "./components/routes/Home";
+import Carrito from "./components/routes/Carrito";
+import Categorias from "./components/routes/Categorias";
 
 function App() {
-
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route element={<Layout/>}>
-      <Route path="/" element={<Home />} />
-      <Route path="/tienda" element={<Tienda />} />
-      <Route path="/carrito" element={<Carrito/>} />
-        </Route>
-    </Routes>
-    </BrowserRouter>
-  </>
-  )
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/carrito" element={<Carrito/>}/>
+            <Route path="/categorias" element={<Categorias/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;

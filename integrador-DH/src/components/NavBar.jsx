aff7535e3d115a6d3ca7e22898f76d14fb6de3cd
TomasTozzi <tozzitomas@gmail.com>
2024-05-14 20:37:18 -0300

    header terminado

--- integrador-DH/src/components/NavBar.jsx
+++ integrador-DH/src/components/NavBar.jsx
@@ -1,75 +1,105 @@
 import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "../styles/index.css";
 import "../styles/navbar.css";
 import logo from "../assets/logo.png";
 
+// function NavBar() {
+
+//   return (
+//     <nav className="navbar navbar-expand-lg ">
+//       <div className="container-fluid ">
+//         <Link to="" className="navbar-brand eslogan" href="#">
+//           <img src={logo} alt="Logo" className="logo" /> ¡Juga más, paga menos!
+//         </Link>
+//         <button
+//           className="navbar-toggler"
+//           type="button"
+//           data-bs-toggle="collapse"
+//           data-bs-target="#navbarNav"
+//           aria-controls="navbarNav"
+//           aria-expanded="false"
+//           aria-label="Toggle navigation"
+//         >
+//           <span className="navbar-toggler-icon"></span>
+//         </button>
+//         <div className="collapse navbar-collapse container-ul " id="navbarNav">
+//           <ul className="navbar-nav space-between">
+//             <li className="nav-item">
+//               <Link
+//                 to="/"
+//                 className="Link nav-link"
+//                 aria-current="page"
+//                 href="#"
+//               >
+//                 Inicio
+//               </Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link to="categorias" className="Link nav-link" href="#">
+//                 Categorias
+//               </Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link to="Carrito" className="Link nav-link" href="#">
+//                 Carrito
+//               </Link>
+//             </li>
+//           </ul>
+
+//           <div className="container-buttons flex  space-between">
+//             <button className=" btn btn-primary ">Iniciar Sesion</button>
+//             <button className=" btn btn-primary ">Registrarse</button>
+//           </div>
+
+//         </div>
+//       </div>
+//     </nav>
+//   );
+
 function NavBar() {
-  const [isVisible, setIsVisible] = useState(true);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth <= 991) {
-        setIsVisible(false);
-      } else {
-        setIsVisible(true);
-      }
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
+  const handleToggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
-    <nav className="navbar navbar-expand-lg ">
-      <div className="container-fluid ">
-        <Link to="" className="navbar-brand eslogan" href="#">
+    <nav className="navbar navbar-expand-lg navbar-light ">
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand eslogan">
           <img src={logo} alt="Logo" className="logo" /> ¡Juga más, paga menos!
         </Link>
         <button
-          className="navbar-toggler borde"
+          className="navbar-toggler"
           type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+          onClick={handleToggleMenu}
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse container-ul " id="navbarNav">
-          <ul className="navbar-nav space-between">
+        <div className={`collapse navbar-collapse ${isMenuOpen ? "show" : ""}`}>
+          <ul className="navbar-nav  me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link
-                to="/"
-                className="Link nav-link"
-                aria-current="page"
-                href="#"
-              >
+              <Link to="/" className="nav-link" aria-current="page">
                 Inicio
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="categorias" className="Link nav-link" href="#">
-                Categorias
+              <Link to="/categorias" className="nav-link">
+                Categorías
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="Carrito" className="Link nav-link" href="#">
+              <Link to="/carrito" className="nav-link">
                 Carrito
               </Link>
             </li>
           </ul>
-        </div>
-        {isVisible && (
-          <div className="container-buttons flex  space-between">
-            <button className="button1 button-general">Iniciar Sesion</button>
-            <button className="button1 button-general">Registrarse</button>
+          <div className="d-flex container-buttons">
+            <button className="btn btn-primary me-2">Iniciar Sesión</button>
+            <button className="btn btn-primary">Registrarse</button>
           </div>
-        )}
+        </div>
       </div>
     </nav>
   );

41099520f280e4b39a4efb2df9d0f5f508680a74
luisoce <luis.campos.escobar@gmail.com>
2024-05-14 23:31:22 -0400

    Modificacion repositorio

--- src/main/java/com/proyectoIntegrador/gameShare/repositorio/ImagenRepositorio.java
+++ src/main/java/com/proyectoIntegrador/gameShare/repositorio/ImagenRepositorio.java
@@ -1,2 +1,10 @@
-package com.proyectoIntegrador.gameShare.repositorio;public interface ImagenRepositorio {
+package com.proyectoIntegrador.gameShare.repositorio;
+
+import com.proyectoIntegrador.gameShare.entidad.Imagen;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImagenRepositorio extends JpaRepository<Imagen, Long>{
+    Imagen save(Imagen imagen);
+
 }
+

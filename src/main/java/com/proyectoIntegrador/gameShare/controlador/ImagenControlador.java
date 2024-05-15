53e6c3ee1df4057b1cb727245f4c91eaf7a3e80c
luisoce <luis.campos.escobar@gmail.com>
2024-05-14 23:20:06 -0400

    Modificación repositorio Imagen

--- src/main/java/com/proyectoIntegrador/gameShare/controlador/ImagenControlador.java
+++ src/main/java/com/proyectoIntegrador/gameShare/controlador/ImagenControlador.java
@@ -1,2 +1,41 @@
-package com.proyectoIntegrador.gameShare.controlador;public class ImagenControlador {
+package com.proyectoIntegrador.gameShare.controlador;
+
+import com.proyectoIntegrador.gameShare.entidad.Imagen;
+import com.proyectoIntegrador.gameShare.repositorio.ImagenRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/imagenes")
+public class ImagenControlador {
+
+    @Autowired
+    private ImagenRepositorio imagenRepositorio;
+
+    @PostMapping("/subir") //Recibe la imagen como un archivo multipart y la guarda en la base de datos.
+    public ResponseEntity<Imagen> subirImagen(@RequestParam("imagen")MultipartFile file) throws IOException {
+        Imagen imagen = new Imagen();
+        imagen.setNombre(file.getOriginalFilename());
+        imagen.setData(file.getBytes());
+
+        Imagen savedImagen = imagenRepositorio.save(imagen);
+        return ResponseEntity.ok(savedImagen);
+    }
+
+    @GetMapping("/{id}")
+    //obtenerImagen= Recupera una imagen por su ID y la devuelve como respuesta HTTP con el tipo de contenido adecuado
+    public ResponseEntity<byte[]> getImagen(@PathVariable Long id) {
+        Imagen imagen = imagenRepositorio.findById(id)
+                .orElseThrow(() -> new RuntimeException("Imagen no encontrada"));
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType(imagen.getNombre().split("\\.")[1]))
+                .body(imagen.getData());
+    }
 }
+
+
+
--- src/main/java/com/proyectoIntegrador/gameShare/entidad/Imagen.java
+++ src/main/java/com/proyectoIntegrador/gameShare/entidad/Imagen.java
@@ -1,2 +1,44 @@
-package com.proyectoIntegrador.gameShare.entidad;public class Imagen {
+package com.proyectoIntegrador.gameShare.entidad;
+
+import jakarta.persistence.*;
+import jdk.jfr.Label;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "Imagenes")
+public class Imagen {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false) //Validación de datos: el nombre no puede ser nulo ni vacio
+    private String nombre;
+    
+    @Lob //indica que el atributo data puede almacenar objetos grandes
+    private byte[] data;
+
+    //Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
 }

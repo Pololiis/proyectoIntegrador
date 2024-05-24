0facc40fbdf1d12223bf164e9e7f67ccb11058bb
JORGE JOAQUIN RIVERA <jorgej.rivera@telefonica.com>
2024-05-23 23:02:37 -0300

    logica de abmplataforma lista para probar

--- integrador-DH/src/App.jsx
+++ integrador-DH/src/App.jsx
@@ -6,6 +6,7 @@ import Categorias from "./components/routes/Categorias";
 import Agregador from "./components/routes/Agregador";
 import DetalleProducto from "./components/routes/DetalleProducto";
 import CrearUsuario from "./components/routes/crearUsuario";
+import AbmPlataforma from "./components/routes/AbmPlataforma";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
             <Route path="/carrito" element={<Carrito/>}/>
             <Route path="/categorias" element={<Categorias/>}/>
             <Route path="/administrador" element={<Agregador/>}/>
+            <Route path="/plataforma" element={<AbmPlataforma/>}/>
             <Route path="/crearUsuario" element={<CrearUsuario/>}/>
             <Route path="/detalleProducto/:id" element={<DetalleProducto/>} />
             <Route path="*" element={<h1>404</h1>} />
---
+++ integrador-DH/src/components/routes/AbmPlataforma.jsx
@@ -0,0 +1,154 @@
+import React, { useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+import "./abmPlataforma.css"; // Estilos personalizados
+
+const AbmPlataforma = () => {
+  const [mensaje, setMensaje] = useState("");
+  const [imagenSeleccionada, setImagenSeleccionada] = useState(null);
+
+  const validationSchema = Yup.object().shape({
+    nombre: Yup.string()
+      .required("El nombre es requerido")
+      .min(2, "El nombre es muy corto")
+      .max(50, "El nombre es muy largo"),
+
+    plataforma: Yup.string().required("La categoría es requerida"),
+    imagen: Yup.mixed().required("Una imagen es requerida"),
+  });
+
+  const handleSubmit = async (values, { setSubmitting, resetForm }) => {
+    const formData = new FormData();
+    formData.append("nombre", values.nombre);
+    if (imagenSeleccionada) {
+      formData.append("imagen", imagenSeleccionada);
+    }
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/plataformas/nueva",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      console.log(response);
+
+      setMensaje("Categoría agregada con éxito");
+      resetForm();
+      setImagenSeleccionada(null);
+    } catch (error) {
+      console.error("Error al crear la categoría:", error);
+      setMensaje("Error al crear la categoría");
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  const handleImagenChange = (event) => {
+    const file = event.currentTarget.files[0];
+    setImagenSeleccionada(file);
+  };
+
+  const handleBorrarImagen = () => {
+    setImagenSeleccionada(null);
+  };
+
+  return (
+    <div className="container my-5 container-form">
+      <div className="restringido row justify-content-center">
+        <div className="col-12 col-md-8">
+          <h2 className="text-center mb-4">No disponible.</h2>
+          <h4 className="text-center mb-6">
+            Esta funcionalidad no está disponible en dispositivos móviles.
+          </h4>
+        </div>
+      </div>
+      <div className="row justify-content-center formulario">
+        <div className="col-12 col-md-8">
+          <h2 className="text-center mb-4">Crear Categoría</h2>
+          <Formik
+            initialValues={{ nombre: "" }}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+          >
+            {({ isSubmitting, errors, touched }) => (
+              <Form>
+                <div className="mb-3">
+                  <label className="form-label">Nombre:</label>
+                  <Field
+                    className={`form-control ${
+                      errors.nombre && touched.nombre
+                        ? "is-invalid"
+                        : touched.nombre
+                        ? "is-valid"
+                        : ""
+                    }`}
+                    type="text"
+                    name="nombre"
+                    placeholder="Nombre"
+                  />
+                  <ErrorMessage
+                    name="nombre"
+                    component="div"
+                    className="text-danger"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label">Imágen:</label>
+                  <input
+                    className={`form-control`}
+                    type="file"
+                    accept="image/*"
+                    onChange={handleImagenChange}
+                  />
+                </div>
+                {imagenSeleccionada && (
+                  <div className="mb-3 img-btn-borrar d-flex">
+                    <div className="imagen-preview borde">
+                      <img
+                        src={URL.createObjectURL(imagenSeleccionada)}
+                        alt="Imagen seleccionada"
+                        className="img-seleccionada"
+                      />
+                      <button
+                        type="button"
+                        className="btn btn-sm btn-danger"
+                        onClick={handleBorrarImagen}
+                      >
+                        Borrar
+                      </button>
+                    </div>
+                  </div>
+                )}
+                <button
+                  className="btn btn-primary w-100"
+                  type="submit"
+                  disabled={isSubmitting}
+                >
+                  Crear Categoría
+                </button>
+              </Form>
+            )}
+          </Formik>
+          {mensaje && (
+            <div
+              className={`alert ${
+                mensaje.includes("éxito") ? "alert-success" : "alert-danger"
+              } mt-4`}
+              role="alert"
+            >
+              {mensaje}
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AbmPlataforma;

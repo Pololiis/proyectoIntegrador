33f9271c4076b847ee1caa40a37659fcd6d450c2
TomasTozzi <tozzitomas@gmail.com>
2024-05-28 14:42:56 -0300

    fix imagenes-producto responsive

--- integrador-DH/src/components/common/CardJuego.jsx
+++ integrador-DH/src/components/common/CardJuego.jsx
@@ -6,7 +6,7 @@ const CardJuego = ({ videojuego }) => {
   return (
     
     
-    <div className="card card-juego mb-3">
+    <div className="card card-juego  borde  mb-3">
       <img src={videojuego.imagenes[0]} className="card-img-top" alt={videojuego.nombre} />
         <hr />
       <div className="card-body flex">
--- integrador-DH/src/components/routes/ListaUsuarios.jsx
+++ integrador-DH/src/components/routes/ListaUsuarios.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
 
 const ListaUsuarios = () => {
 // Estado inicial con un array de plataformas

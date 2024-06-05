441a6a35ddf55a218ab04e52282f38ebda6abd66
TomasTozzi <tozzitomas@gmail.com>
2024-06-05 18:51:03 -0300

    buscador ya filtra bien por nombre

--- integrador-DH/src/components/Main.jsx
+++ integrador-DH/src/components/Main.jsx
@@ -3,33 +3,11 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "../styles/main.css";
 import BarraBuscador from "./common/BarraBuscador";
-import CardJuego from "./common/CardJuego";
+
 import CardPlataforma from "./common/CardPlataforma";
 
 
 function Main() {
-  const url = `http://localhost:8080/videojuegos`;
-  const [videoJuegos, setVideoJuegos] = useState([]);
-
-
-  const [cantidad, setCantidad] = useState(10);
-
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(url);
-
-        const shuffledData = shuffleArray(response.data);
-        setVideoJuegos(shuffledData);
-      } catch (error) {
-        console.error("Hubo un error al hacer la solicitud:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
 
   return (
     <div>
@@ -53,31 +31,11 @@ function Main() {
       <div className="container-cards-section flex">
         <h2>Plataformas</h2>
         <CardPlataforma />
-        <BarraBuscador />
-
-        <section className="cards-recomendado">
-          <h2>Recomendados</h2>
-          <div className="container-cards   flex">
-            {videoJuegos.slice(0, cantidad).map((videojuego) => (
-              <CardJuego key={videojuego.id} videojuego={videojuego} />
-            ))}
-          </div>
-        </section>
-
+        <BarraBuscador  />
       </div>
     </div>
   );
 }
 
-
-// Función de barajado
-function shuffleArray(array) {
-  const shuffledArray = array.slice(); // Crear una copia del array para no mutar el original
-  for (let i = shuffledArray.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
-  }
-  return shuffledArray;
-}
-
 export default Main;
+
--- integrador-DH/src/components/common/BarraBuscador.jsx
+++ integrador-DH/src/components/common/BarraBuscador.jsx
@@ -1,17 +1,51 @@
-
 import "./buscador.css";
-
-import React from 'react';
-
+import CardJuego from "./CardJuego";
+import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function BarraBuscador() {
+  const url = `http://localhost:8080/videojuegos`;
+  const [videoJuegos, setVideoJuegos] = useState([]);
+  const [cantidad, setCantidad] = useState(10);
+  const [busqueda, setBusqueda] = useState("");
+  const [isSubmiting, setIsSubmiting] = useState(false);
+  const [filteredData, setFilteredData] = useState([]);
+
+  const handleSearch = (e) => {
+    setBusqueda(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsSubmiting(true);
+    const filtered = videoJuegos.filter((videojuego) =>
+      videojuego.nombre.toLowerCase().includes(busqueda.toLowerCase())
+    );
+    setFilteredData(filtered);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(url);
+        const shuffledData = shuffleArray(response.data);
+        setVideoJuegos(shuffledData);
+      } catch (error) {
+        console.error("Hubo un error al hacer la solicitud:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="container buscador">
       <div className="row justify-content-center">
-
-        <div className="col-sm-12 col-md-6 col-lg-4">
+        <div className="col-sm-12 col-md-6 col-lg-6">
           <div className="input-group mb-2 buscador-pequeno">
             <input
+              onChange={handleSearch}
               type="text"
               className="form-control buscador-input"
               placeholder="Buscar..."
@@ -19,6 +53,7 @@ function BarraBuscador() {
               aria-describedby="button-addon2"
             />
             <button
+              onClick={handleSubmit}
               className="btn btn-buscador btn-bd-primary"
               type="button"
               id="button-addon2"
@@ -28,9 +63,33 @@ function BarraBuscador() {
           </div>
         </div>
       </div>
+      <section className="cards-recomendado">
+        <h2>Recomendados</h2>
+        {isSubmiting ? (
+          <div className="container-cards flex">
+            {filteredData.slice(0, cantidad).map((videojuego) => (
+              <CardJuego key={videojuego.id} videojuego={videojuego} />
+            ))}
+          </div>
+        ) : (
+          <div className="container-cards flex">
+            {videoJuegos.slice(0, cantidad).map((videojuego) => (
+              <CardJuego key={videojuego.id} videojuego={videojuego} />
+            ))}
+          </div>
+        )}
+      </section>
     </div>
   );
 }
 
 export default BarraBuscador;
 
+function shuffleArray(array) {
+  const shuffledArray = array.slice(); // Crear una copia del array para no mutar el original
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  }
+  return shuffledArray;
+}

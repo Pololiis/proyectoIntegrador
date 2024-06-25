f4b7d15e89fd44f3beb22d3a094286e2e285cfe9
TomasTozzi <tozzitomas@gmail.com>
2024-06-25 15:39:12 -0300

    se corrigio pequeño error en reserva

--- integrador-DH/src/components/routes/DetalleReserva.jsx
+++ integrador-DH/src/components/routes/DetalleReserva.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Container, Grid, Paper, Typography, Button, Modal, Box } from "@mui/material";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import axios from 'axios';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
@@ -32,6 +32,7 @@ function DetalleReserva() {
   const [usuario, setUsuario] = useState(null);
   const location = useLocation();
   const navigate = useNavigate();
+  const { id } = useParams();
   const { token, updateToken } = useAuthContext();
   const url = `http://localhost:8080/alquiler`;
 
@@ -110,7 +111,7 @@ const handleDateChange = async () => {
         const response = await axios.put(`${url}/${id}`, {
             fechaInicio: startDate.toISOString().split("T")[0],
             fechaFin: endDate.toISOString().split("T")[0],
-            usuariosId: { id: usuario.id }, // Asegúrate de pasar solo el id del usuario existente
+            usuariosId: usuario.id , // Asegúrate de pasar solo el id del usuario existente
             videojuegosId: videoJuegoSeleccionado.id
         }, {
             headers: {
